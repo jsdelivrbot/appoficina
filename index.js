3d8dc91dbff30256ac7fe21b46f36a5ecaf6a0da
= <fferro@orion.tech>
2018-10-26 15:30:25 -0300

    correção dos dados de banco de dados e router

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = require('express').Router();
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000), router)
 app.use(express.static(__dirname + '/public'))
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
