be5e08c1ec5b2c4ca8bd7c78173f428e3ef1542d
= <fferro@orion.tech>
2018-11-12 16:27:00 -0200

    teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = require('express').Router();
 var app = express();
 
-app.set('port', (process.env.port || 3000))
+app.set('port', (process.env.port || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
